7260e5175d5d414b3451f0ca523e8da559a47d48
Vasissualiyp <vpustovoit@cita.utoronto.ca>
2024-03-28 13:51:53 -0400

    Modularized loading of paper"

--- bin/download_paper.sh
+++ bin/download_paper.sh
@@ -2,43 +2,23 @@
 
 source ~/env/venv/bin/activate
 
+parent_dir="$1"
+bibtex_entry="$2"
+source_download="$3"
+
 # Configuration
-parent_dir="$HOME/research/references"
 csv_file="$parent_dir/papers.csv"
 bibtex_file="$parent_dir/bibliography.bib"
 pdf_dir="$parent_dir"
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PYTHON_DIR="$SCRIPT_DIR"
-
-# Function to fetch BibTeX entry
-fetch_bibtex() {
-    pybibget "$1"
-}
-
-obtain_bibtex_info() {
-    bibtex_entry="$1"
-    echo "$bibtex_entry"
-    author=$(echo "$bibtex_entry" | grep author | awk '{print $3}' | tr -cd '[:alpha:]') #Get only last name of the 1st author
-    title=$(echo "$bibtex_entry" | grep title | awk '{print $3}' | tr -cd '[:alpha:]') #Get only the 1st title word
-    year=$(echo "$bibtex_entry" | grep year | awk '{print $3}' | tr -cd '[0-9]') #Get only the year
-    full_author=$(echo "$bibtex_entry" | grep author | awk '{ $1=$2=""; sub(/^  */, ""); print }' | tr -cd '[:alpha:] ') #Get only last name of the 1st author
-    full_title=$(echo "$bibtex_entry" | grep title | awk '{ $1=$2=""; sub(/^  */, ""); print }' | tr -cd '[:alpha:] ') #Get only last name of the 1st author
-}
-
-print_bibtex_info() {
-    bibtex_entry="$1"
-	obtain_bibtex_info "$bibtex_entry"
-    echo "Full Title: $full_title"
-    echo "Full Author: $full_author"
-    echo "year: $year"
-}
+BIN_DIR="$SCRIPT_DIR"
 
 download_paper_and_source() {
     bibtex_entry="$1"
     source_download="$2"
 
-    label=$(create_label "$author" "$title" "$year")
 	echo "$label"
     download_name="$label.pdf"
 
@@ -70,16 +50,6 @@ download_paper_and_source() {
     fi
 
     echo "Paper processing completed."
-}
-
-# Function to create a short reference label
-create_label() {
-    read -p "Enter a label (press Enter for default): " user_label
-	if [ -z "$user_label" ]; then
-      echo "$1$2$3" 
-    else
-      echo "$user_label"
-    fi
 }
 
 # Change the titular bibtex reference from arxiv number to the label
@@ -130,22 +100,7 @@ update_csv() {
 
 # Main script execution
 main() {
-    read -p "Enter the arXiv number: " arxiv_num
-    bibtex_entry=$(fetch_bibtex "$arxiv_num")
-    print_bibtex_info "$bibtex_entry"
-    
-    read -p "Do you want to download this paper? (Y/n): " response
-	if [ -z "$response" ]; then
-		response="y"
-    fi
-
-    read -p "Would you like to download the LaTeX source for this paper? (y/N): " source_download
-    
-    if [[ $response =~ ^[Yy]$ ]]; then
-        download_paper_and_source "$bibtex_entry" "$source_download"
-    else
-        echo "Download cancelled."
-    fi
+    download_paper_and_source "$bibtex_entry" "$source_download"
 }
 
 main
---
+++ bin/download_paper_wrapper.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+
+source ~/env/venv/bin/activate
+
+# Configuration
+parent_dir="$HOME/research/references"
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PYTHON_DIR="$SCRIPT_DIR"
+BIN_DIR="$SCRIPT_DIR"
+
+# Function to fetch BibTeX entry
+fetch_bibtex() {
+    pybibget "$1"
+}
+
+obtain_bibtex_info() {
+    bibtex_entry="$1"
+    echo "$bibtex_entry"
+    author=$(echo "$bibtex_entry" | grep author | awk '{print $3}' | tr -cd '[:alpha:]') #Get only last name of the 1st author
+    title=$(echo "$bibtex_entry" | grep title | awk '{print $3}' | tr -cd '[:alpha:]') #Get only the 1st title word
+    year=$(echo "$bibtex_entry" | grep year | awk '{print $3}' | tr -cd '[0-9]') #Get only the year
+    full_author=$(echo "$bibtex_entry" | grep author | awk '{ $1=$2=""; sub(/^  */, ""); print }' | tr -cd '[:alpha:] ') #Get only last name of the 1st author
+    full_title=$(echo "$bibtex_entry" | grep title | awk '{ $1=$2=""; sub(/^  */, ""); print }' | tr -cd '[:alpha:] ') #Get only last name of the 1st author
+}
+
+print_bibtex_info() {
+    bibtex_entry="$1"
+	obtain_bibtex_info "$bibtex_entry"
+    echo "Full Title: $full_title"
+    echo "Full Author: $full_author"
+    echo "year: $year"
+}
+
+# Function to create a short reference label
+create_label() {
+    read -p "Enter a label (press Enter for default): " user_label
+	if [ -z "$user_label" ]; then
+      echo "$1$2$3" 
+    else
+      echo "$user_label"
+    fi
+}
+
+# Main script execution
+main() {
+    read -p "Enter the arXiv number: " arxiv_num
+    bibtex_entry=$(fetch_bibtex "$arxiv_num")
+    print_bibtex_info "$bibtex_entry"
+    
+    read -p "Do you want to download this paper? (Y/n): " response
+	if [ -z "$response" ]; then
+		response="y"
+    fi
+
+    read -p "Would you like to download the LaTeX source for this paper? (y/N): " source_download
+
+    label=$(create_label "$author" "$title" "$year")
+    
+    if [[ $response =~ ^[Yy]$ ]]; then
+        "$BIN_DIR/download_paper.sh" "$parent_dir" "$bibtex_entry" "$source_download"
+    else
+        echo "Download cancelled."
+    fi
+}
+
+main
