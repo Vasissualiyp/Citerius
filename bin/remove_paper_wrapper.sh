9b6d901f95a2859aca86c935d0d9ebed70f7fa3a
Vasissualiyp <vpustovoit@cita.utoronto.ca>
2025-05-02 18:33:31 -0400

    Fully pythonized paper removal

--- bin/remove_paper_wrapper.sh
+++ bin/remove_paper_wrapper.sh
@@ -6,15 +6,5 @@ parent_dir="$HOME/research/references" # Directory containing reference material
 # Determine script directory for relative path operations
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PYTHON_DIR="$SCRIPT_DIR/python"
-CITERIUS_DIR="$SCRIPT_DIR/.."
-BIN_DIR="$SCRIPT_DIR"
 
-main_wrapper() {
-    # Use fzf to select a paper from the CSV file, ignoring the header line
-    local label=$($PYTHON_DIR/config.py "$parent_dir")
-	echo "$label"
-	$BIN_DIR/remove_paper.sh "$parent_dir" "$label"
-}
-
-# Call the main function to start the script
-main_wrapper
+python $PYTHON_DIR/remove_paper.py "$parent_dir"
